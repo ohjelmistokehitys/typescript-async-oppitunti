1e78b892883bcf16ccbbb7d0ca378df993be3602
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2023-03-03 19:20:43 +0000

    Minor speed improvement

--- src/palindromes/palindrome.ts
+++ src/palindromes/palindrome.ts
@@ -17,10 +17,35 @@ export function isPalindrome(text: string): boolean {
 }
 
 export function* findTwoWordPalindromes(words: string[]): Generator<string> {
-    for (let w1 of words) {
-        for (let w2 of words) {
-            if (isPalindrome(w1 + w2)) {
-                yield w1 + w2;
+    /* Palindromes start and end with the same character, so we create two
+     * maps to group words by their first and last chars. */
+    let byFirstChar: Record<string, string[]> = {};
+    let byLastChar: Record<string, string[]> = {};
+
+    for (let w of words) {
+        let first = w.at(0)!;
+        let last = w.at(-1)!;
+
+        // create new arrays if the char appears first time:
+        byFirstChar[first] ??= [];
+        byLastChar[last] ??= [];
+
+        // store word in arrays based on the first and last chars:
+        byFirstChar[first].push(w);
+        byLastChar[last].push(w);
+    }
+
+    /* For each char, check if there are pairs in the first and last char arrays
+     * that make up a palindrome. */
+    for (let char of Object.keys(byFirstChar)) {
+        let firstWords = byFirstChar[char];
+        let secondWords = byLastChar[char] ?? [];
+
+        for (let w1 of firstWords) {
+            for (let w2 of secondWords) {
+                if (isPalindrome(w1 + w2)) {
+                    yield w1 + w2;
+                }
             }
         }
     }
